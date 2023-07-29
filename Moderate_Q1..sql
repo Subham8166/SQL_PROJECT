905f957d3b572e96b0ca9551ce9982da34a923a1
Subham Sundar Moharana <136557186+Subham8166@users.noreply.github.com>
2023-07-29 20:57:17 +0530

    Add files via upload

---
+++ Moderate_Q1..sql
@@ -0,0 +1,10 @@
+select first_name,last_name,email
+from customer
+join invoice on customer.customer_id = invoice.customer_id
+join invoice_line on invoice.invoice_id = invoice_line.invoice_id
+where track_id in(
+	select track_id from track
+	join genre on track.genre_id = genre.genre_id
+	where genre.name like 'Rock'
+)
+order by email asc;
